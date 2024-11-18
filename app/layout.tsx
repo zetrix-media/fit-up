9de19d04f03dbb0a9d4541983c4d4ccd2b19eaa2
skarticsinha <skarticsinha@gmail.com>
2024-11-18 20:08:02 +0530

    added page seed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
