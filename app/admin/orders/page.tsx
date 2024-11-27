cd1be73f395f41afa74354790263c703a043c432
skarticsinha <skarticsinha@gmail.com>
2024-11-27 19:37:13 +0530

    orders page

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FaBoxOpen } from 'react-icons/fa';
 
 const OrdersPage = () => {
   return (
-    <div>
-      <h1>Coming Soon</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <FaBoxOpen className="text-6xl text-gray-400 mb-4" />
+      <h1 className="text-2xl font-bold text-gray-700">No Orders...</h1>
     </div>
   );
 };
