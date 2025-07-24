9328a10ab61d93b5800aedadbcea538707f39d5a
skarticsinha <skarticsinha@gmail.com>
2025-07-24 10:04:47 +0530

    Updated sidebar component

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -6,13 +6,19 @@ import Sidebar from '@/components/Sidebar';
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
-  // Exclude Sidebar on the login page
   const isLoginPage = pathname === '/admin/login';
 
   return (
-    <div className="flex">
+    <div className="relative min-h-screen">
       {!isLoginPage && <Sidebar />}
-      <main className={`flex-1 ${isLoginPage ? '' : 'p-4'}`}>{children}</main>
+
+      <main
+        className={`${
+          isLoginPage ? '' : 'ml-64'
+        } h-screen overflow-y-auto p-4`}
+      >
+        {children}
+      </main>
     </div>
   );
 }
